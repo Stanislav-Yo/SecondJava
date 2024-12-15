e2e861a55c3ca81c1f964d7dfafbc42daffc874d
Stas Git <yolkhingp@gmail.com>
2024-12-15 18:16:00 +0300

    Task_1

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+        int amount = 22000;
+        int perMile = 20;
+        int bonus = amount / perMile;
+
+        System.out.println("Итоговый бонус: " + bonus);
+
+        System.out.println("Bye!");
+    }
+}
